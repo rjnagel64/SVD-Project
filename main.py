48d4275bb181916788865ac287db5a696796320f
Robert J Nagel <rjnagel64@gmail.com>
2018-06-15 11:06:41 -0500

    Create some empty functions

--- main.py
+++ main.py
@@ -9,5 +9,18 @@ from Bio import SeqIO
 def main():
     print("Hello, World!")
 
+
+def get_data():
+    pass
+
+def translate_sequences():
+    pass
+
+def extract_proteins():
+    pass
+
+def create_matrix():
+    pass
+
 if __name__ == "__main__":
     main()
