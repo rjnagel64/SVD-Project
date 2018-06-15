319640e395faaf84511d421e83b8579d46e30903
Robert J Nagel <rjnagel64@gmail.com>
2018-06-15 11:47:08 -0500

    translate sequences

--- main.py
+++ main.py
@@ -4,23 +4,33 @@
 import Bio
 from Bio import Seq
 from Bio import SeqIO
+import os
 
 
 def main():
     print("Hello, World!")
+
+    translate_sequences()
 
 
 def get_data():
     pass
 
 def translate_sequences():
-    pass
+    filenames = os.listdir("dna-sequences")
+    for fn in filenames:
+        for (i, record) in enumerate(SeqIO.parse(f"dna-sequences/{fn}", "fasta")):
+            SeqIO.write(record.translate(), f"aa-sequences/{i}-{fn}", "fasta")
 
 def extract_proteins():
-    pass
+    filenames = os.listdir("aa-sequences")
+    for fn in filenames:
+        record = next(SeqIO.parse(f"aa-sequences/{fn}", "fasta"))
+        # TODO: Do some stuff with this. Actually get the protein sequences out of it.
 
 def create_matrix():
     pass
+
 
 if __name__ == "__main__":
     main()
